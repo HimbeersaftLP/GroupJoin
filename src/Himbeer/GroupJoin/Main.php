e3266eaa438ef6407a9d96dbeaa8da4298638b9d
HimbeersaftLP <mail@himbeer.me>
2019-09-05 16:54:38 +0200

    Initial Commit

---
+++ src/Himbeer/GroupJoin/Main.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Himbeer\GroupJoin;
+
+use _64FF00\PurePerms\PurePerms;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+	/** @var bool */
+	private $hideOther;
+
+	/** @var string[][] */
+	private $messages;
+
+	/** @var PurePerms */
+	private $purePerms;
+
+	public function onEnable(): void {
+		$this->saveDefaultConfig();
+		$this->hideOther = (bool)$this->getConfig()->get("hide-other");
+		$this->messages = $this->getConfig()->get("groups");
+
+		$this->purePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	private function getGroupNameForPlayer(Player $player): string {
+		$ppGroup = $this->purePerms->getUserDataMgr()->getGroup($player);
+		if ($ppGroup === null) {
+			// This should never happen, if it does, the server owner messed up their PurePerms config
+			// We don't need to log this, PurePerms does that already
+			return "";
+		}
+		return $ppGroup->getName();
+	}
+
+	private function getMessageForPlayer(Player $player, string $type): ?string {
+		$groupName = $this->getGroupNameForPlayer($player);
+		if (!isset($this->messages[$groupName])) {
+			return null;
+		}
+		$messages = $this->messages[$groupName];
+		if (!isset($messages[$type])) {
+			return null;
+		}
+		return str_replace("{player}", $player->getName(), $messages[$type]);
+	}
+
+	public function onJoin(PlayerJoinEvent $event) {
+		$message = $this->getMessageForPlayer($event->getPlayer(), "join");
+		if ($message) {
+			$event->setJoinMessage($message);
+		} else if ($this->hideOther) {
+			$event->setJoinMessage("");
+		}
+	}
+
+	public function onQuit(PlayerQuitEvent $event) {
+		$message = $this->getMessageForPlayer($event->getPlayer(), "leave");
+		if ($message) {
+			$event->setQuitMessage($message);
+		} else if ($this->hideOther) {
+			$event->setQuitMessage("");
+		}
+	}
+}
